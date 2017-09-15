4ad2850b71e01193f4c1c0c74dccba2951460e93
Anders Pearson <anders@thraxil.org>
2017-09-15 10:53:48 +0100

    simple test for newEndpoint()

--- endpoint_test.go
+++ endpoint_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 	"testing"
+
+	"github.com/go-kit/kit/log"
 )
 
 func Test_metricsFromMap(t *testing.T) {
@@ -21,4 +24,25 @@ func Test_metricsFromMap(t *testing.T) {
 		t.Error("wrong number of metrics found")
 	}
 
+}
+
+func dummyLogger() log.Logger {
+	devNull, _ := os.Open("/dev/null")
+	w := log.NewSyncWriter(devNull)
+	logger := log.NewJSONLogger(w)
+	return logger
+}
+
+func Test_newEndpoint(t *testing.T) {
+	c := endpointconfig{
+		URL:           "http://example.com/",
+		Prefix:        "test",
+		CheckInterval: 60,
+		Timeout:       60,
+	}
+	g := newGraphiteServer("1.2.3.4", 2003)
+	e := newEndpoint(c, 60, 60, g, httpFetcher{}, dummyLogger())
+	if e.url != c.URL {
+		t.Error("lost the URL")
+	}
 }
