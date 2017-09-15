b890f55668b57a096458766f014803a5f5d259c1
Anders Pearson <anders@thraxil.org>
2017-09-15 19:22:22 +0100

    test Fetch()

--- endpoint_test.go
+++ endpoint_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/go-kit/kit/log"
@@ -67,4 +71,60 @@ func Test_Submit(t *testing.T) {
 	if err != nil {
 		t.Error("dummy submitter shouldn't return an error")
 	}
+}
+
+type dummyFetcher struct{}
+
+type dummyReadCloser struct {
+	body io.ReadSeeker
+}
+
+func (d *dummyReadCloser) Read(p []byte) (n int, err error) {
+	n, err = d.body.Read(p)
+	if err == io.EOF {
+		d.body.Seek(0, 0)
+	}
+	return n, err
+}
+
+func (d *dummyReadCloser) Close() error {
+	return nil
+}
+
+func NewRespBodyFromString(body string) io.ReadCloser {
+	return &dummyReadCloser{strings.NewReader(body)}
+}
+
+func (d dummyFetcher) Get(ctx context.Context, url string) (*http.Response, error) {
+	data := `{"foo": 10, "bar": 5}`
+
+	r := &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		Body:       NewRespBodyFromString(data),
+		Header:     http.Header{},
+	}
+	return r, nil
+}
+
+func Test_Fetch(t *testing.T) {
+	c := endpointconfig{
+		URL:           "http://example.com/",
+		Prefix:        "test",
+		CheckInterval: 60,
+		Timeout:       60,
+	}
+	g := newGraphiteServer("1.2.3.4", 2003)
+	e := newEndpoint(c, 60, 60, g, dummyFetcher{}, dummyLogger())
+
+	ctx := context.TODO()
+	d, err := e.Fetch(ctx)
+	if err != nil {
+		t.Error("dummy fetcher shouldn't fail")
+	}
+	metrics := metricsFromMap(d, "")
+	if len(metrics) != 2 {
+		t.Error("wrong number of metrics found")
+	}
+
 }
